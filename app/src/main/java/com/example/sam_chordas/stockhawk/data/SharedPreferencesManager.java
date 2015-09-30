2053467e91fcc3660bf50c574551586563bad25f
schordas <samec39@gmail.com>
2015-09-30 13:46:56 -0700

    first commit

---
+++ app/src/main/java/com/example/sam_chordas/stockhawk/data/SharedPreferencesManager.java
@@ -0,0 +1,13 @@
+package com.example.sam_chordas.stockhawk.data;
+
+import android.content.Context;
+
+/**
+ * Created by sam_chordas on 9/30/15.
+ */
+public class SharedPreferencesManager {
+  private Context context;
+  public SharedPreferencesManager(Context context){
+    this.context = context;
+  }
+}
---
+++ app/src/main/java/com/example/sam_chordas/stockhawk/service/StockTaskService.java
@@ -0,0 +1,7 @@
+package com.example.sam_chordas.stockhawk.service;
+
+/**
+ * Created by sam_chordas on 9/30/15.
+ */
+public class StockTaskService {
+}
